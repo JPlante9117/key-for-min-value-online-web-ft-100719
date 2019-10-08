8d8a5a73a2ee92c374be331afee9fe52e7d4dbc8
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-08 13:02:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,6 +10,9 @@ def key_for_min_value(name_hash)
       name_hash.collect do |name, number|
         if number < min_value
           name
+          if name_hash.collect.length > 1
+            name_hash.collect.shift
+          end
         end
       end
   end
