7b9897ed6773f851fba4bf7a1bc2e322d5ee8dee
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-08 13:13:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@ require 'pry'
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value = 1000000
+  min_value = name_hash[key]
   key = nil
   if name_hash == {}
     return nil 
