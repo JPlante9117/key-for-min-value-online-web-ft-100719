fcd431fbb5c045fbf0d9b071c42bdae8a8250e8b
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-08 12:58:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,5 +3,11 @@ require 'pry'
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash == {}
+    return nil 
+  else
+      name_hash.collect do |name, number|
+        
+      end
+  end
 end
